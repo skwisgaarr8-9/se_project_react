3d35fab66d54b3dd36a69f236fc6807d010dbb52
Francis Flanagan <7flanaganf@gmail.com>
2023-04-03 07:26:08 +0900

    fix: updated import statement

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,5 @@
 import "./Profile.css";
-import SideBar from "../Sidebar/Sidebar";
+import SideBar from "../SideBar/SideBar";
 import ClothesSection from "../ClothesSection/ClothesSection";
 
 function Profile({ cards, weatherData, handleCardClick, handleAddCardClick }) {
